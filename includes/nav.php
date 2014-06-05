81008d1985c4de3b5c57ce4719b818be54053674
karlosos <karlososhd@gmail.com>
2014-06-05 23:11:20 +0200

    JavaScript 3

--- includes/nav.php
+++ includes/nav.php
@@ -5,6 +5,34 @@
  * @author Karol Dzialowski
  */
 ?>
-<nav> 
-    <?php echo layout::getMenu() ?>
+<script>
+    /**
+    * Funkcja odkrywa/ukrywa widok kalkulatora delty
+    */
+   function odkryj_div()
+   {    
+//       if (document.getElementById("nav").style.visibility == "hidden") {
+//           document.getElementById("nav").style.visibility = "visible";
+//       }
+//       else if (document.getElementById("nav").style.visibility == "visible") {
+//           document.getElementById("nav").style.visibility = "hidden";
+//       }
+       
+       document.getElementById("nav").style.backgroundColor == "#02343F";
+       
+       if (document.getElementById("nav").style.visibility == "hidden") {
+           document.getElementById("nav").style.visibility = "visible";
+        }
+       else if (document.getElementById("nav").style.visibility == "visible") {
+           document.getElementById("nav").style.visibility = "hidden";
+        }
+       
+       console.log("test");
+
+   }
+   
+</script>
+<nav id="nav"> 
+        <?php echo layout::getMenu() ?>
 </nav>
+    <button onclick="odkryj_div();">Ukryj/Odkryj menu</button>
